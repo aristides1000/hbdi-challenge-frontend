1b46b70bf81d9b588dc32f2db946a76679948fca
aristides1000 <aristides1000@gmail.com>
2022-03-16 14:15:29 -0400

    Show all names

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import '../css/Search.css';
 import { InputGroup, FormControl, Button, Form } from 'react-bootstrap';
 import { BiSearchAlt } from 'react-icons/bi';
+import { allLists } from '../redux/list/list';
 
 const Search = () => {
+  const dispatch = useDispatch();
+  const allListsAction = bindActionCreators(allLists, dispatch);
+  const lists = useSelector((state) => state.lists.allLists);
+  const loading = useSelector((state) => state.lists.loading);
+
+  useEffect(async () => {
+    if (loading) {
+      await allListsAction();
+    }
+  }, []);
+
+  const renderLists = () => {
+    const result = [];
+    for (let i = 0; i < lists.length; i += 1) {
+      result.push(
+        <tr key={i}>
+          <td>{lists[i].name}</td>
+        </tr>,
+      );
+    }
+    return result;
+  };
+
   return (
     <div className="container-fluid">
       <div className="row m-5">
@@ -31,9 +57,7 @@ const Search = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Mark</td>
-            </tr>
+            {!loading && renderLists()}
           </tbody>
         </table>
       </div>
